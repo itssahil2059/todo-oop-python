66f5c4bf378ea6388568b2c064228239444646e4
calculator <itssahil2059@gmail.com>
2026-03-25 00:12:16 -0500

    Updated todo app and added random.py

---
+++ random.py
@@ -0,0 +1,10 @@
+# try:
+#     number = int(input("Enter a number: "))
+#.    print(f"You entered: {number}")
+    #   if number % 10 == 0:
+    #         print("The number is a multiple of 10")
+# except ValueError:
+#     print("Please enter a valid number")
+
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
